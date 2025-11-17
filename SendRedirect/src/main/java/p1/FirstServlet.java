c8e6f6575c189d5693d4bb30c0cee8b525401a18
Pratikraje Bhosale <pratikrajebhosale07@gmail.com>
2025-11-17 23:57:30 +0530

    send redirect example

---
+++ SendRedirect/src/main/java/p1/FirstServlet.java
@@ -0,0 +1,40 @@
+package p1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<h1>"+"I am FirstServlet"+"</h1>");
+		
+		response.sendRedirect("https://www.amazon.in/");
+	}
+
+	
+
+}
